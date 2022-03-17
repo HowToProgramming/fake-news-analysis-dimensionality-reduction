a61bae72c505c9249f111ac5c507299f5c92b4f2
IndexError <lkycst@gmail.com>
2022-03-17 21:44:08 +0700

    Refactor

--- main.py
+++ main.py
@@ -5,40 +5,51 @@ from sklearn.decomposition import PCA
 
 from dataset import get_news_dataset
 
-def tfidf_pca(dataset, label, ndim=2):
-    tfidf_text = TfidfVectorizer()
-    result = tfidf_text.fit_transform(dataset)
-    word_list = tfidf_text.vocabulary_
-    word_inv = {v: k for k, v in word_list.items()}
-    result = result.toarray()
-    pca_text = PCA(n_components=ndim)
-    pca_result = pca_text.fit_transform(result)
-    fake_result = pca_result[np.where(label == 0)]
-    real_result = pca_result[np.where(label == 1)]
-    eigenvector = pca_text.components_
-    maxes = np.argsort(eigenvector, axis=1)[:, :5]
-    max_word = []
-    for max_values in maxes:
-        a = []
-        for idx in max_values:
-            a.append(word_inv[idx])
-        max_word.append(a.copy())
-    print(max_word)
+class TfIdf_PCA():
+    def __init__(self, pca_ndim=2):
+        self.pca_ndim = pca_ndim
+        self.tf_idf_vectorizer = TfidfVectorizer()
+        self.PCA = PCA(n_components=self.pca_ndim)
+    
+    def fit_transform(self, X):
+        tf_idf_vectors = self.tf_idf_vectorizer.fit_transform(X)
+        tf_idf_vectors = tf_idf_vectors.toarray()
+        pca_vectors = self.PCA.fit_transform(tf_idf_vectors)
+        return pca_vectors
+    
+    def pca_eigenvector(self):
+        return self.PCA.components_
+    
+    def predict(self, X):
+        tf_idf_vectors = self.tf_idf_vectorizer.transform(X)
+        tf_idf_vectors = tf_idf_vectors.toarray()
+        pca_vectors = self.PCA.transform(tf_idf_vectors)
+        return pca_vectors
+    
+    def fit(self, X):
+        self.fit_transform(X)
+
+def plot_news(pca_result, label):
+    fake_result = pca_result[label == 0]
+    real_result = pca_result[label == 1]
     plt.figure()
     plt.title('Fake News vs. Real News analysis using PCA')
     plt.scatter(fake_result[:, 0], fake_result[:, 1], c='r', label='fake news')
     plt.scatter(real_result[:, 0], real_result[:, 1], c='g', label='real news')
     plt.legend()
 
-fake_news, real_news = get_news_dataset()
+if __name__ == "__main__":
+    fake_news, real_news = get_news_dataset()
 
-data_limit = 1000
-fake_news = fake_news[:data_limit]
-real_news = real_news[:data_limit]
+    data_limit = 500
+    fake_news = fake_news[:data_limit]
+    real_news = real_news[:data_limit]
 
-tfidf_pca(np.concatenate((fake_news, real_news)), np.concatenate((np.zeros(len(fake_news)), np.ones(len(real_news)))))
-plt.savefig('fake_real.png')
-plt.show()
+    documents = np.concatenate((fake_news, real_news))
+    labels = np.concatenate((np.zeros(len(fake_news)), np.ones(len(real_news))))
 
-# This model
-# Compare with state of the art using k-fold cross validation
+    model = TfIdf_PCA()
+    result = model.fit_transform(documents)
+
+    plot_news(result, labels)
+    plt.show()
